94b97179815cd970305727e4a4fb3cd3f538dda9
Serede Sixty Six <serede.dev@gmail.com>
2018-09-07 01:07:55 +0200

    Code refactored. Bottle app now triggers update.

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import sqlite3
 import bottle
 from bottle import route, run, template, static_file
 
-from call_history import DB_FILE, TABLE_OUT_NAME, TABLE_IN_NAME, TABLE_TIME_COLNAME
+from call_history import update_call_history
+from call_history import DB_FILE, OUTGOING_TABLE, INCOMING_TABLE, TIME_COLUMN
 
 
 bottle.TEMPLATE_PATH.insert(0, 'template')
@@ -13,12 +14,13 @@ bottle.TEMPLATE_PATH.insert(0, 'template')
 
 @route('/')
 def call_history():
+    update_call_history()
     conn = sqlite3.connect(DB_FILE)
     c = conn.cursor()
-    c.execute('SELECT * FROM ' + TABLE_OUT_NAME + ' ORDER BY ' + TABLE_TIME_COLNAME)
+    c.execute('SELECT * FROM ' + OUTGOING_TABLE + ' ORDER BY ' + TIME_COLUMN)
     outgoing_hdr = list(map(lambda x: x[0].replace('_', ' '), c.description))
     outgoing = c.fetchall()[::-1]
-    c.execute('SELECT * FROM ' + TABLE_IN_NAME + ' ORDER BY ' + TABLE_TIME_COLNAME)
+    c.execute('SELECT * FROM ' + INCOMING_TABLE + ' ORDER BY ' + TIME_COLUMN)
     incoming_hdr = list(map(lambda x: x[0].replace('_', ' '), c.description))
     incoming = c.fetchall()[::-1]
     c.close()
--- call_history.py
+++ call_history.py
@@ -13,41 +13,18 @@ _DBG_ = False
 
 IP = '192.168.1.1'
 LOGIN_URL = 'http://{ip}/login-login.cgi'.format(**{'ip': IP})
-CHO_URL = 'http://{ip}/voip/CallHistoryOutgoing.html'.format(**{'ip': IP})
-CHI_URL = 'http://{ip}/voip/CallHistoryIncoming.html'.format(**{'ip': IP})
+OUTGOING_URL = 'http://{ip}/voip/CallHistoryOutgoing.html'.format(**{'ip': IP})
+INCOMING_URL = 'http://{ip}/voip/CallHistoryIncoming.html'.format(**{'ip': IP})
 USER = '1234'
-PASSWD_FILE = '.passwd'
+PW_FILE = '.passwd'
 DB_FILE = 'call_history.db'
 TABLE_INDEX = -1
-TABLE_OUT_NAME = 'outgoing'
-TABLE_IN_NAME = 'incoming'
-TABLE_TIME_COLNAME = 'time'
-
-# Fetch tables
-
-with open(PASSWD_FILE) as file:
-    sessionKey = base64.b64encode(
-        '{user}:{pass}'.format(**{
-            'user': USER,
-            'pass': file.readline().rstrip('\n')
-        }).encode()
-    )
-
-data = {
-    'sessionKey': sessionKey,
-    'pass': ''
-}
-
-session = requests.Session()
-
-login_response = session.post(LOGIN_URL, data=data)
-
-if _DBG_:
-    print(login_response.headers)
-    print(session.cookies.get_dict())
+OUTGOING_TABLE = 'outgoing'
+INCOMING_TABLE = 'incoming'
+TIME_COLUMN = 'time'
 
 
-def read_table(session, url):
+def _read_table(session, url):
     response = session.get(url)
     soup = BeautifulSoup(response.content, 'lxml')
     if _DBG_:
@@ -55,33 +32,64 @@ def read_table(session, url):
     table = soup.find_all('table')[TABLE_INDEX]
     df = pandas.read_html(str(table), header=0, index_col=0)[0]
     df.columns = df.columns.str.replace(' ', '_')
-    df[TABLE_TIME_COLNAME] = pandas.to_datetime(df[TABLE_TIME_COLNAME]).astype(str)
+    df[TIME_COLUMN] = pandas.to_datetime(df[TIME_COLUMN]).astype(str)
     return df
 
 
-cho = read_table(session, CHO_URL)
-chi = read_table(session, CHI_URL)
+def _get_tables():
+    with open(PW_FILE) as file:
+        sessionKey = base64.b64encode(
+            '{user}:{pass}'.format(**{
+                'user': USER,
+                'pass': file.readline().rstrip('\n')
+            }).encode()
+        )
 
-if _DBG_:
-    print(cho)
-    print(chi)
+    data = {
+        'sessionKey': sessionKey,
+        'pass': ''
+    }
 
-# Database
+    session = requests.Session()
 
-engine = create_engine('sqlite:///' + DB_FILE, echo=_DBG_)
+    login_response = session.post(LOGIN_URL, data=data)
 
-metadata = MetaData()
+    if _DBG_:
+        print(login_response.headers)
+        print(session.cookies.get_dict())
 
-outgoing = Table(TABLE_OUT_NAME, metadata,
-                 *[Column(name, String, nullable=False) for name in list(cho)],
-                 UniqueConstraint(*list(cho), name='uix'))
-incoming = Table(TABLE_IN_NAME, metadata,
-                 *[Column(name, String, nullable=False) for name in list(chi)],
-                 UniqueConstraint(*list(cho), name='uix'))
+    och = _read_table(session, OUTGOING_URL)
+    ich = _read_table(session, INCOMING_URL)
 
-metadata.create_all(engine)
+    if _DBG_:
+        print(och)
+        print(ich)
 
-cho.apply(lambda x: engine.execute(
-    outgoing.insert().prefix_with('OR IGNORE').values(x)), axis=1)
-chi.apply(lambda x: engine.execute(
-    incoming.insert().prefix_with('OR IGNORE').values(x)), axis=1)
+    return (och, ich)
+
+
+def _update_database(och, ich):
+    engine = create_engine('sqlite:///' + DB_FILE, echo=_DBG_)
+
+    metadata = MetaData()
+
+    outgoing = Table(OUTGOING_TABLE, metadata,
+                     *[Column(name, String, nullable=False)
+                       for name in list(och)],
+                     UniqueConstraint(*list(och), name='uix'))
+    incoming = Table(INCOMING_TABLE, metadata,
+                     *[Column(name, String, nullable=False)
+                       for name in list(ich)],
+                     UniqueConstraint(*list(och), name='uix'))
+
+    metadata.create_all(engine)
+
+    och.apply(lambda x: engine.execute(
+        outgoing.insert().prefix_with('OR IGNORE').values(x)), axis=1)
+    ich.apply(lambda x: engine.execute(
+        incoming.insert().prefix_with('OR IGNORE').values(x)), axis=1)
+
+
+def update_call_history():
+    tables = _get_tables()
+    _update_database(*tables)
