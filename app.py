ddfecbcaf26ef489b8a06085ab68000ec74f25ec
Sergio Fuentes de Uña <serede.dev@gmail.com>
2025-01-19 12:45:07 +0100

    Switch to Poetry and update code.

--- app.py
+++ app.py
@@ -1,43 +1,54 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
 import sqlite3
+import sys
+
 import bottle
-from bottle import route, run, template, static_file
+from bottle import route, run, static_file, template
 
-from call_history import update_call_history
-from call_history import DB_FILE, OUTGOING_TABLE, INCOMING_TABLE, TIME_COLUMN
+from call_history import (
+    DB_FILE,
+    INCOMING_TABLE,
+    OUTGOING_TABLE,
+    TIME_COLUMN,
+    update_call_history,
+)
+
+bottle.TEMPLATE_PATH.insert(0, "template")
 
 
-bottle.TEMPLATE_PATH.insert(0, 'template')
-
-
-@route('/')
+@route("/")
 def call_history():
     update_call_history()
     conn = sqlite3.connect(DB_FILE)
     c = conn.cursor()
-    c.execute('SELECT * FROM ' + OUTGOING_TABLE + ' ORDER BY ' + TIME_COLUMN)
-    outgoing_hdr = list(map(lambda x: x[0].replace('_', ' '), c.description))
+    c.execute("SELECT * FROM " + OUTGOING_TABLE + " ORDER BY " + TIME_COLUMN)
+    outgoing_hdr = list(map(lambda x: x[0].replace("_", " "), c.description))
     outgoing = c.fetchall()[::-1]
-    c.execute('SELECT * FROM ' + INCOMING_TABLE + ' ORDER BY ' + TIME_COLUMN)
-    incoming_hdr = list(map(lambda x: x[0].replace('_', ' '), c.description))
+    c.execute("SELECT * FROM " + INCOMING_TABLE + " ORDER BY " + TIME_COLUMN)
+    incoming_hdr = list(map(lambda x: x[0].replace("_", " "), c.description))
     incoming = c.fetchall()[::-1]
     c.close()
     template_dict = {
-        'outgoing_hdr': outgoing_hdr,
-        'outgoing': outgoing,
-        'incoming_hdr': incoming_hdr,
-        'incoming': incoming
+        "outgoing_hdr": outgoing_hdr,
+        "outgoing": outgoing,
+        "incoming_hdr": incoming_hdr,
+        "incoming": incoming,
     }
-    output = template('table', **template_dict)
+    output = template("table", **template_dict)
     return output
 
 
 # Static Routes
-@route('/<filepath:path>')
+@route("/<filepath:path>")
 def server_static(filepath):
-    return static_file(filepath, root='template')
+    return static_file(filepath, root="template")
 
 
-run(host='0.0.0.0', port=8008, debug=True, reloader=True)
+def main() -> int:
+    run(host="0.0.0.0", port=8008, debug=True, reloader=True)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
--- call_history.py
+++ call_history.py
@@ -1,37 +1,39 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
 import base64
-import requests
+import os
+from io import StringIO
+
 import pandas
+import requests
 from bs4 import BeautifulSoup
-from sqlalchemy import create_engine, MetaData, Table, Column, String, UniqueConstraint
+from sqlalchemy import Column, MetaData, String, Table, UniqueConstraint, create_engine
 
 # Constants
 
-_DBG_ = False
+_DBG_ = os.getenv("CALL_HISTORY_DBG", False)
 
-IP = '192.168.1.1'
-LOGIN_URL = 'http://{ip}/login-login.cgi'.format(**{'ip': IP})
-OUTGOING_URL = 'http://{ip}/voip/CallHistoryOutgoing.html'.format(**{'ip': IP})
-INCOMING_URL = 'http://{ip}/voip/CallHistoryIncoming.html'.format(**{'ip': IP})
-USER = '1234'
-PW_FILE = '.passwd'
-DB_FILE = 'call_history.db'
+IP = "192.168.1.1"
+LOGIN_URL = "http://{ip}/login-login.cgi".format(**{"ip": IP})
+OUTGOING_URL = "http://{ip}/voip/CallHistoryOutgoing.html".format(**{"ip": IP})
+INCOMING_URL = "http://{ip}/voip/CallHistoryIncoming.html".format(**{"ip": IP})
+USER = "1234"
+PW_FILE = ".passwd"
+DB_FILE = "call_history.db"
 TABLE_INDEX = -1
-OUTGOING_TABLE = 'outgoing'
-INCOMING_TABLE = 'incoming'
-TIME_COLUMN = 'time'
+OUTGOING_TABLE = "outgoing"
+INCOMING_TABLE = "incoming"
+TIME_COLUMN = "time"
 
 
 def _read_table(session, url):
     response = session.get(url)
-    soup = BeautifulSoup(response.content, 'lxml')
+    soup = BeautifulSoup(response.content, "lxml")
     if _DBG_:
         print(soup.prettify())
-    table = soup.find_all('table')[TABLE_INDEX]
-    df = pandas.read_html(str(table), header=0, index_col=0)[0]
-    df.columns = df.columns.str.replace(' ', '_')
+    table = soup.find_all("table")[TABLE_INDEX]
+    df = pandas.read_html(StringIO(str(table)), header=0, index_col=0)[0]
+    df.columns = df.columns.str.replace(" ", "_")
     df[TIME_COLUMN] = pandas.to_datetime(df[TIME_COLUMN]).astype(str)
     return df
 
@@ -39,16 +41,12 @@ def _read_table(session, url):
 def _get_tables():
     with open(PW_FILE) as file:
         sessionKey = base64.b64encode(
-            '{user}:{pass}'.format(**{
-                'user': USER,
-                'pass': file.readline().rstrip('\n')
-            }).encode()
+            "{user}:{pass}".format(
+                **{"user": USER, "pass": file.readline().rstrip("\n")}
+            ).encode()
         )
 
-    data = {
-        'sessionKey': sessionKey,
-        'pass': ''
-    }
+    data = {"sessionKey": sessionKey, "pass": ""}
 
     session = requests.Session()
 
@@ -69,26 +67,33 @@ def _get_tables():
 
 
 def _update_database(och, ich):
-    engine = create_engine('sqlite:///' + DB_FILE, echo=_DBG_)
+    engine = create_engine("sqlite:///" + DB_FILE, echo=_DBG_)
 
     metadata = MetaData()
 
-    outgoing = Table(OUTGOING_TABLE, metadata,
-                     *[Column(name, String, nullable=False)
-                       for name in list(och)],
-                     UniqueConstraint(*list(och), name='uix'))
-    incoming = Table(INCOMING_TABLE, metadata,
-                     *[Column(name, String, nullable=False)
-                       for name in list(ich)],
-                     UniqueConstraint(*list(och), name='uix'))
+    outgoing = Table(
+        OUTGOING_TABLE,
+        metadata,
+        *[Column(name, String, nullable=False) for name in list(och)],
+        UniqueConstraint(*list(och), name="uix")
+    )
+    incoming = Table(
+        INCOMING_TABLE,
+        metadata,
+        *[Column(name, String, nullable=False) for name in list(ich)],
+        UniqueConstraint(*list(och), name="uix")
+    )
 
     metadata.create_all(engine)
 
-    for o in och.itertuples(index=False):
-        engine.execute(outgoing.insert().prefix_with('OR IGNORE').values(o))
+    with engine.connect() as connection:
+        for o in och.itertuples(index=False):
+            connection.execute(outgoing.insert().prefix_with("OR IGNORE").values(o))
 
-    for i in ich.itertuples(index=False):
-        engine.execute(incoming.insert().prefix_with('OR IGNORE').values(i))
+        for i in ich.itertuples(index=False):
+            connection.execute(incoming.insert().prefix_with("OR IGNORE").values(i))
+
+        connection.commit()
 
 
 def update_call_history():
@@ -98,4 +103,3 @@ def update_call_history():
 
 if __name__ == "__main__":
     update_call_history()
-
